da110d19aec421a3c1faa65bff5ba954665f9642
nilamtandel-spec <nilam.tandel@sandipuniversity.edu.in>
2026-04-28 17:18:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,6 +235,10 @@ def logout():
 # ===============================
 import os
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
