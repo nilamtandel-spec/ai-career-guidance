3e83124030b969b3b80da18c6bdb838d437c6c16
nilamtandel-spec <nilam.tandel@sandipuniversity.edu.in>
2026-04-28 16:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 app.secret_key = "careerpilot_secret_key"
 
 
